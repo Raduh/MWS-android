6356d2b06c1a66f34010f6b56bfe43656d498e12
Radu Hambasan <radu.hambasan@gmail.com>
2014-10-05 11:22:55 +0200

    Put progress circle on top of WebView

--- app/src/main/java/kwarc/com/mathwebsearch/ResultsActivity.java
+++ app/src/main/java/kwarc/com/mathwebsearch/ResultsActivity.java
@@ -22,7 +22,6 @@ public class ResultsActivity extends Activity {
         String qText = creator.getStringExtra(MainActivity.EXTRA_TEXT);
         String qLatex = creator.getStringExtra(MainActivity.EXTRA_LATEX);
 
-
         WebView webview = (WebView) findViewById(R.id.results_webvw);
         webview.getSettings().setJavaScriptEnabled(true);
         webview.getSettings().setBuiltInZoomControls(true);
